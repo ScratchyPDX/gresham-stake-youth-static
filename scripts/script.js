534a15a78cf7e39d969cb374e65538124f466d24
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2024-12-30 11:23:17 -0800

    Add button click events

--- scripts/script.js
+++ scripts/script.js
@@ -37,10 +37,44 @@ const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
 const downloadCalendarPdf = document.getElementById('download-calendar-pdf');
+const downloadYwGeneralCampInfoPdf = document.getElementById('download-yw-general-camp-info-pdf');
+const downloadYwPackingListPdf = document.getElementById('download-yw-packing-list-pdf');
+const downloadGeneralYmCampInfoForParentsPdf = document.getElementById('download-general-ym-camp-info-for-parents-pdf');
+const downloadYmCampPackingListPdf = document.getElementById('download-ym-camp-packing-list-pdf');
+const downloadBishopricYmCampRepresentativePdf = document.getElementById('download-bishopric-ym-camp-representative-pdf');
 
 if(downloadCalendarPdf != null) {
   downloadCalendarPdf.addEventListener("click", () =>  {
     alert("You are about to download the calendar PDF");
   });
 }
-  
+ 
+if(downloadYwGeneralCampInfoPdf != null) {
+  downloadYwGeneralCampInfoPdf.addEventListener("click", () =>  {
+    alert("You are about to download the YM Camp General Info PDF");
+  });
+}
+
+if(downloadYwPackingListPdf != null) {
+  downloadYwPackingListPdf.addEventListener("click", () =>  {
+    alert("You are about to download the YM Camp Packing List PDF");
+  });
+}
+
+if(downloadGeneralYmCampInfoForParentsPdf != null) {
+  downloadGeneralYmCampInfoForParentsPdf.addEventListener("click", () =>  {
+    alert("You are about to download the YM Camp Info for Parents PDF");
+  });
+}
+
+if(downloadYmCampPackingListPdf != null) {
+  downloadYmCampPackingListPdf.addEventListener("click", () =>  {
+    alert("You are about to download the YM Camp Packing List PDF");
+  });
+}
+
+if(downloadBishopricYmCampRepresentativePdf != null) {
+  downloadBishopricYmCampRepresentativePdf.addEventListener("click", () =>  {
+    alert("You are about to download the Bishopric and YM Camp Info PDF");
+  });
+}
